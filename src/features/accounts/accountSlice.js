20a20bab9f2b5573a82cb115f065aa8b3a8cda0d
Satyam Pund <pundsatyam@gmail.com>
2024-03-16 16:23:30 +0530

    dispatching action from react

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,7 +1,7 @@
 const initialStateAccount = {
   balance: 0,
   loan: 0,
-  loanPurspose: '',
+  loanPurpose: '',
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
@@ -15,11 +15,11 @@ export default function accountReducer(state = initialStateAccount, action) {
       return {
         ...state,
         loan: action.payload.amount,
-        loanPurspose: action.payload.purpose,
+        loanPurpose: action.payload.purpose,
         balance: state.balance + action.payload.amount,
       };
     case 'account/payLoan':
-      return { ...state, loan: 0, loanPurspose: '', balance: state.balance - state.loan };
+      return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan };
     default:
       return state;
   }
