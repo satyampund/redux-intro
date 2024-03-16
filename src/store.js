03c3d44cd812e69052c94eff221f091918669104
Satyam Pund <pundsatyam@gmail.com>
2024-03-16 15:24:00 +0530

    redux file structure: state slice

--- src/store.js
+++ src/store.js
@@ -1,96 +1,9 @@
 import { combineReducers, createStore } from 'redux';
-
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurspose: '',
-};
-
-const initialStateCustomer = {
-  fullName: '',
-  nationalID: '',
-  createdAt: '',
-};
-
-function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case 'account/deposit':
-      return { ...state, balance: state.balance + action.payload };
-    case 'account/withdraw':
-      return { ...state, balance: state.balance - action.payload };
-    case 'account/requestLoan':
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurspose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case 'account/payLoan':
-      return { ...state, loan: 0, loanPurspose: '', balance: state.balance - state.loan };
-    default:
-      return state;
-  }
-}
-
-function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case 'customer/createCustomer':
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case 'customer/updateName':
-      return { ...state, fullName: action.payload };
-    default:
-      return state;
-  }
-}
+import accountReducer from './features/accounts/accountSlice';
+import customerReducer from './features/customers/customerSlice';
 
 const rootReducer = combineReducers({ account: accountReducer, customer: customerReducer });
 
 const store = createStore(rootReducer);
 
-console.log('Hey redux.....');
-
-function deposit(amount) {
-  return { type: 'account/deposit', payload: amount };
-}
-
-function withdraw(amount) {
-  return { type: 'account/withdraw', payload: amount };
-}
-
-function requestLoan(amount, purpose) {
-  return { type: 'account/requestLoan', payload: { amount, purpose } };
-}
-
-function payLoan() {
-  return { type: 'account/payLoan' };
-}
-
-store.dispatch(deposit(500));
-store.dispatch(withdraw(200));
-store.dispatch(requestLoan(1000, 'Buy a Car!'));
-store.dispatch(payLoan());
-
-function createCustomer(fullName, nationalID) {
-  return {
-    type: 'customer/createCustomer',
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-
-function updateName(fullName) {
-  return {
-    type: 'customer/updateName',
-    payload: fullName,
-  };
-}
-
-store.dispatch(createCustomer('Virat Kohli', 'India'));
-store.dispatch(updateName('King Kohli'));
-
-console.log(store.getState());
+export default store;
